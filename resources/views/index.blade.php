7984bac9ea7985d981c1ecd196fe7c0fa04280a8
Vinicius Albano <vinialbano@gmail.com>
2017-08-26 03:13:10 -0300

    Index prototype

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,18 +18,63 @@
 </div>
 <div class="site-container">
     <div class="row justify-content-center">
-        <div class="col-8">
-            <form action="">
-                <div class="form-group">
+        <div class="toolbar">
+            <form action="" class="toolbar-form">
+                <div class="form-row">
+                    <div class="input-group-addon">Filtros</div>
+                    <select class="filter-select" id="inlineFormInputGroupUsername">
+                        <option selected>Escolha o filtro</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
                     <input type="text" class="form-control" placeholder="Digite aqui">
+                    {{--<button class="btn btn-primary">Buscar</button>--}}
                 </div>
+                <button class="statistics-btn">Estatísticas</button>
             </form>
         </div>
     </div>
 
-</div>
+    <article class="article">
+        <div class="jumbotron">
+            <h1 class="display-3">Jumbotron heading</h1>
+            <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+        </div>
+
+        <div class="row marketing">
+            <div class="col-lg-6">
+                <h4>Subheading</h4>
+                <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                <h4>Subheading</h4>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                <h4>Subheading</h4>
+                <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+            </div>
+
+            <div class="col-lg-6">
+                <h4>Subheading</h4>
+                <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                <h4>Subheading</h4>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                <h4>Subheading</h4>
+                <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+            </div>
+        </div>
+    </article>
 </div>
 
 <script src="js/app.js"></script>
+<script>
+    $(".statistics-btn").click((element) => {
+        element.preventDefault();
+        $(".map").toggleClass("is-hidden");
+    });
+</script>
 </body>
 </html>
