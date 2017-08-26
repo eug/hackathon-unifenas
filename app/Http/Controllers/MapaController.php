f1b91c78b6d797a0ec64f1a1ebe1877af292a415
Maurício Vidal <mauriciovidalxc@hotmail.com>
2017-08-26 09:11:57 -0300

    Saiba mais modificado!

--- app/Http/Controllers/MapaController.php
+++ app/Http/Controllers/MapaController.php
@@ -27,10 +27,6 @@ class MapaController extends Controller {
 
     public function criarMarcadores() {
         $arq = file(base_path() . "/dados/latlon.csv");
-//        
-//        foreach($arq as $line){
-//            $data[] = str_getcsv($line, $delimiter = ';');
-//        }
         for ($i = 0; $i < sizeof($arq); $i++) {
             $data[] = str_getcsv($arq[$i], $delimiter = ',');
             if ($i == 0) {
@@ -39,22 +35,25 @@ class MapaController extends Controller {
             $lat = $data[$i][27];
             $lng = $data[$i][28];
             $cnpj = $data[$i][4];
-            $fantasia = $data[$i][10] == ""? $data[$i][15]: $data[$i][10];
+            $fantasia = $data[$i][10] == "" ? $data[$i][15] : $data[$i][10];
             $fundacao = $data[$i][6];
             $telefone = $data[$i][20];
+            $email = $data[$i][9];
+            $area = $data[$i][24];
             $doc = $data[$i][26];
-            
+
             $content = "<h6> $fantasia </h6></br>"
-                      ."<b> CNPJ:</b> $cnpj</br>"
-                      ."<b> Fundação: </b> $fundacao </br>";
-            if($telefone != ""){
+                    . "<b> CNPJ:</b> $cnpj</br>"
+                    . "<b> Fundação: </b> $fundacao </br>";
+            if ($telefone != "") {
                 $content .= "<b>Telefone: </b> $telefone</br>";
             }
-            $content .= "</br><a href='/$doc'> Saiba Mais </a>";
-            
-            
-            
-            
+            $content .= "</br><button id = 'saibaMais' onClick = 'saibaMais()'> Saiba Mais </button>"
+                       ."<div id='bloco' style='display: none'> "
+                       ."<b> Email:</b> $email </br>"
+                       . "<b> Area de Atuação: </b>  $area </br> "
+                       . "</div>";
+
             Mapper::informationWindow($lat, $lng, $content);
         }
     }
@@ -65,14 +64,65 @@ class MapaController extends Controller {
         }
     }
 
-    public function teste() {
-        //Mapper::marker(53.381128999999990000, -1.470085000000040000);
-        //Mapper::marker(53.381128999999990000, -1.470085000000040000, ['symbol' => 'circle', 'scale' => 1000]);
-        //Mapper::map(-21.4716, -46.3341);
-        //Mapper::marker(-21.7883,-46.5625, ['markers' => ['symbol' => 'circle', 'scale' => 1000, 'animation' => 'DROP']]);
-        Mapper::marker(-21.7883, -46.5625, ['icon' => 'https://d30fqisedfc4ds.cloudfront.net/rensupv1/images/rss-icon.png']);
+    public function saibaMais($doc_) {
+        $arq_ = file(base_path() . "/dados/latlon.csv");
+        $empresa = [];
+        //Procura empresa
+        for ($i = 0; $i < sizeof($arq_); $i++) {
+            $data[] = str_getcsv($arq_[$i], $delimiter = ',');
+            if ($i == 0) {
+                continue;
+            }
+            $doc = $data[$i][26];
+            //echo "[$i]".$doc." - ".$doc_."</br>";
+            if ($doc == $doc_) {
+                //echo "entrou</br>";
+                $nome = $data[$i][10] == "" ? $data[$i][15] : $data[$i][10];
+                $endereco = $data[$i][11] . ", " . $data[$i][16] . " - " . $data[$i][1];
+                $empresa = ['nome' => $nome, 'endereco' => $endereco,
+                    'fundacao' => $data[$i][6], 'telefone' => $data[$i][20],
+                    'email' => $data[$i][9], 'lat' => $data[$i][27], 'lng' => $data[$i][28],
+                    'area' => $data[$i][24], 'cnpj' => $data[$i][4]];
+                break;
+            }
+        }
+        //echo "Achou</br>";
+        //Marca empresas com a mesma area
+        $arq = file(base_path() . "/dados/latlon.csv");
+        for ($i = 0; $i < sizeof($arq); $i++) {
+            $data[] = str_getcsv($arq[$i], $delimiter = ',');
+            if ($i == 0) {
+                continue;
+            }
 
-        return view('map');
+            $doc = $data[$i][26];
+            echo "[$i]".$doc." - ".$doc_."</br>";
+            echo "$arq[$i]<\br>";
+            if ($doc_ == $doc) {
+                echo "entrou</br>";
+                Mapper::marker($empresa['lat'], $empresa['lng'], ['draggable' => false, 'icon' => 'https://support.xbox.com/Content/Images/live_status_active_icon.png']);
+                continue;
+            }
+            $area = $data[$i][24];
+            if ($area == $empresa['area']) {
+                $lat = $data[$i][27];
+                $lng = $data[$i][28];
+                $cnpj = $data[$i][4];
+                $fantasia = $data[$i][10] == "" ? $data[$i][15] : $data[$i][10];
+                $fundacao = $data[$i][6];
+                $telefone = $data[$i][20];
+                $doc = $data[$i][26];
+
+                $content = "<h4> $fantasia </h4></br>"
+                        . "<b> CNPJ:</b> $cnpj</br>"
+                        . "<b> Fundação: </b> $fundacao </br>";
+                if ($telefone != "") {
+                    $content .= "<b>Telefone: </b> $telefone</br>";
+                }
+                Mapper::informationWindow($lat, $lng, $content);
+            }
+        }
+        return view('saibamais', $empresa);
     }
 
 }
